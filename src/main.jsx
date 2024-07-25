b631e1d58b8599f9b11185aee69d472e09d1155b
Zahra Rajabi <zahra.80.rajabi@gmail.com>
2024-07-25 03:57:18 +0330

    Final Commit

--- src/main.jsx
+++ src/main.jsx
@@ -7,6 +7,18 @@ import theme from "./mui/theme.js";
 import "./styles/index.css";
 import "./styles/font.css";
 import { BrowserRouter } from "react-router-dom";
+import { useEffect } from "react";
+import { useLocation } from "react-router-dom";
+
+export default function ScrollToTop() {
+  const { pathname } = useLocation();
+
+  useEffect(() => {
+    window.scrollTo(0, 0);
+  }, [pathname]);
+
+  return null;
+}
 
 const client = new ApolloClient({
   uri: import.meta.env.VITE_GRAPHCMS_URI,
@@ -14,13 +26,14 @@ const client = new ApolloClient({
 });
 
 ReactDOM.createRoot(document.getElementById("root")).render(
-  // <React.StrictMode>
+  <React.StrictMode>
   <BrowserRouter>
     <ApolloProvider client={client}>
       <ThemeProvider theme={theme}>
+        <ScrollToTop />
         <App />
       </ThemeProvider>
     </ApolloProvider>
   </BrowserRouter>
-  // </React.StrictMode>
+  </React.StrictMode>
 );
